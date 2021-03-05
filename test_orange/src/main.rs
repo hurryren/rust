32d411f7d2da65aa208d333e8f0db2c6645a8115
orange <823renj@gmail.com>
2021-03-05 21:58:11 +0800

    update rust learn

--- test_orange/src/main.rs
+++ test_orange/src/main.rs
@@ -1,62 +1,76 @@
 use std::sync::mpsc;
+use std::sync::{Mutex,Arc};
 use std::thread;
 use std::time::Duration;
-use std::sync::Mutex;
+use std::rc::Rc;
 
 
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
 
-fn main(){
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles{
+        println!("num: {:?}",counter);
+        handle.join().unwrap();
+    }
+}
+
+fn mute_test1() {
     let m = Mutex::new(5);
 
     {
         let mut num = m.lock().unwrap();
         *num = 6;
-        
     }
 
-    println!("m = {:?}",m);
-
+    println!("m = {:?}", m);
 }
 
 fn thread_test() {
-    let (tx,rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
     let tx1 = mpsc::Sender::clone(&tx);
 
     thread::spawn(move || {
-        let vals =vec![
+        let vals = vec![
             String::from("hi"),
             String::from("from"),
             String::from("the"),
             String::from("thread"),
         ];
-        for val in vals{
+        for val in vals {
             tx.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
     thread::spawn(move || {
-        let vals =vec![
+        let vals = vec![
             String::from("tx1 hi"),
             String::from("tx1 from"),
             String::from("tx1 the"),
             String::from("tx1 thread"),
         ];
-        for val in vals{
+        for val in vals {
             tx1.send(val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
 
-
-
-    for received in rx{
-        println!("Got: {:?}",received);
+    for received in rx {
+        println!("Got: {:?}", received);
     }
 
     // handle.join().unwrap();
-    
-
-    
 }
