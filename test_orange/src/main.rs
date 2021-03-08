5ff1b3bf4ebfc627aae6f68b3968f2419c4504a1
orange <823renj@gmail.com>
2021-03-08 20:41:03 +0800

    update

--- test_orange/src/main.rs
+++ test_orange/src/main.rs
@@ -5,7 +5,92 @@ use std::time::Duration;
 use std::rc::Rc;
 
 
-fn main() {
+fn largest_T<T>(list:&[T]) ->T{
+    let mut largest = list[0];
+
+    for &item in list.iter(){
+        if item > largest{
+            largest = item;
+        }
+    }
+    largest
+}
+
+
+fn main(){
+    let number_list = vec![23,34,667,5,1];
+
+    let result = largest_T(&number_list);
+    println!("the largest number is {}",result);
+
+
+    let char_list = vec!['y','d','a','x'];
+
+    let result = largest_T(&char_list);
+    println!("the largest char is {}",result);
+
+}
+
+fn largest_char(list:&[char]) -> char{
+    let mut largest = list[0];
+
+    for &item in list.iter(){
+        if item > largest{
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn largest_i32(list:&[i32]) -> i32{
+    let mut largest = list[0];
+
+    for &item in list.iter(){
+        if item > largest{
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+
+fn main1()
+{
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+}
+
+fn largest(list: &[i32]) ->i32{
+    let mut largest = list[0];
+
+    for &item in list{
+        if item > largest{
+            largest = item;
+        }
+    }
+
+    largest
+
+}
+
+
+
+
+
+
+
+
+fn mute_test2() {
     let counter = Arc::new(Mutex::new(0));
     let mut handles = vec![];
 
